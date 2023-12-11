fb1e34914b0d4f83ef3df5c2b3622d2a58a0d987
haibinh.phan <haibinh.phan@shopee.com>
2023-12-11 17:07:27 +0700

    feat: multiple teams

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from "./logo.png";
 import "./App.css";
-import { Card, Form, InputNumber, Select, Button, message } from "antd";
+import { Card, Form, InputNumber, Select, Button, message, Switch } from "antd";
 import { CHAMPIONS, CHAMPION_IMAGES } from "./data";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { CopyOutlined } from "@ant-design/icons";
 import useCheckMobileScreen from "./useCheckMobileScreen";
 
@@ -23,33 +23,43 @@ const random = (n, ignoredList) => {
 
 function App() {
   const [form] = Form.useForm();
-  const q = Form.useWatch("quantity", form);
-  const quantity = parseInt(q, 10);
-  const b = Form.useWatch("bonus", form);
-  const bonus = parseInt(b, 10) || 0;
+  const quantity = parseInt(Form.useWatch("quantity", form), 10);
+  const bonus = parseInt(Form.useWatch("bonus", form), 10) || 0;
+  const isMultipleTeam = Form.useWatch("mode", form);
+  const team = parseInt(Form.useWatch("team", form), 10) || 2;
 
   const [champions, setChampions] = useState([]);
   const [championsQuantity, setChampionsQuantity] = useState(undefined);
   const [championsBonus, setChampionsBonus] = useState(0);
+  const [numberOfTeams, setNumberOfTeams] = useState(undefined);
 
   const isMobile = useCheckMobileScreen();
 
-  const teams = [
-    champions.slice(0, championsQuantity),
-    champions.slice(championsQuantity, championsQuantity * 2 + championsBonus),
-  ];
+  const teams = useMemo(() => {
+    const result = [];
+    for (let i = 0; i < numberOfTeams; i++) {
+      const cur = champions.slice(
+        championsQuantity * i,
+        championsQuantity * (i + 1) +
+          (i === numberOfTeams - 1 ? championsBonus : 0)
+      );
+      result.push(cur);
+    }
+    return result;
+  }, [champions, championsBonus, championsQuantity, numberOfTeams]);
 
   const onRandom = useCallback(
     (values) => {
-      setChampions(random(quantity * 2 + bonus, values.banned || []));
+      setChampions(random(quantity * team + bonus, values.banned || []));
       setChampionsQuantity(quantity);
       setChampionsBonus(bonus);
+      setNumberOfTeams(team);
     },
-    [bonus, quantity]
+    [bonus, team, quantity]
   );
 
-  const copyChampions = (team) => {
-    navigator.clipboard.writeText(teams[team].join(", "));
+  const copyChampions = (t) => {
+    navigator.clipboard.writeText(teams[t].join(", "));
     message.success("Copied!");
   };
 
@@ -73,14 +83,32 @@ function App() {
         <div>Random LoL Champions</div>
       </nav>
       <div className="main">
-        <Card style={{ width: "90%", maxWidth: "1180px" }}>
+        <Card style={{ width: "90%", maxWidth: "1280px" }}>
           <Form layout="inline" onFinish={onRandom} form={form}>
+            <Form.Item name="mode" valuePropName="checked">
+              <Switch checkedChildren="Teams" unCheckedChildren="Default" />
+            </Form.Item>
             <Form.Item label="Quantity of Champions" name="quantity">
-              <InputNumber placeholder="Exp: 10" min={5} max={40} />
+              <InputNumber placeholder="Exp: 10" min={5} max={20} />
             </Form.Item>
-            <Form.Item label="Weak team bonus" name="bonus">
-              <InputNumber placeholder="Exp: 2" min={0} max={20} />
-            </Form.Item>
+            {isMultipleTeam ? (
+              <Form.Item
+                label="Number of teams"
+                name="team"
+                style={{ width: 220 }}
+              >
+                <InputNumber placeholder="Exp: 2" min={2} max={8} />
+              </Form.Item>
+            ) : (
+              <Form.Item
+                label="Weak team bonus"
+                name="bonus"
+                style={{ width: 220 }}
+              >
+                <InputNumber placeholder="Exp: 2" min={0} max={20} />
+              </Form.Item>
+            )}
+
             <Form.Item
               label="Banned Champions"
               name="banned"
@@ -111,7 +139,7 @@ function App() {
                 quantity === undefined ||
                 Number.isNaN(quantity) ||
                 quantity < 5 ||
-                quantity > 40
+                quantity > 20
               }
             >
               Random
@@ -122,6 +150,7 @@ function App() {
                 setChampionsQuantity(undefined);
                 setChampions([]);
                 setChampionsBonus(0);
+                setNumberOfTeams(undefined);
               }}
               style={{ marginLeft: 12 }}
             >
@@ -129,71 +158,73 @@ function App() {
             </Button>
           </Form>
         </Card>
-        <Card style={{ width: "90%", maxWidth: "1180px", flexGrow: 1 }}>
+        <Card style={{ width: "90%", maxWidth: "1280px", flexGrow: 1 }}>
           {championsQuantity !== undefined && (
             <div>
-              {[0, 1].map((team) => (
-                <div style={{ paddingTop: 20, paddingBottom: 20 }}>
-                  <div>
-                    <span
+              {Array.from({ length: numberOfTeams }, (_, index) => index).map(
+                (t) => (
+                  <div style={{ paddingTop: 20, paddingBottom: 20 }}>
+                    <div>
+                      <span
+                        style={{
+                          fontWeight: "bold",
+                          fontSize: 24,
+                        }}
+                      >
+                        Team {t + 1}{" "}
+                      </span>
+                      <CopyOutlined
+                        style={{ fontSize: 20, cursor: "pointer" }}
+                        onClick={copyChampions.bind(this, t)}
+                      />
+                    </div>
+                    <div
                       style={{
-                        fontWeight: "bold",
-                        fontSize: 24,
+                        display: "flex",
+                        gap: 12,
+                        flexWrap: "wrap",
+                        justifyContent: "space-between",
                       }}
                     >
-                      Team {team + 1}{" "}
-                    </span>
-                    <CopyOutlined
-                      style={{ fontSize: 20, cursor: "pointer" }}
-                      onClick={copyChampions.bind(this, team)}
-                    />
-                  </div>
-                  <div
-                    style={{
-                      display: "flex",
-                      gap: 12,
-                      flexWrap: "wrap",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    {teams[team].map((champion) => (
-                      <Card
-                        style={{
-                          width: 140,
-                          height: 162,
-                        }}
-                        bodyStyle={{ padding: 0 }}
-                      >
-                        <img
-                          width={138}
-                          height={138}
-                          src={CHAMPION_IMAGES[champion]}
-                          alt={champion}
-                          key={champion}
+                      {teams[t].map((champion) => (
+                        <Card
                           style={{
-                            borderTopLeftRadius: 8,
-                            borderTopRightRadius: 8,
-                            objectFit: "cover",
-                            verticalAlign: "middle",
+                            width: 140,
+                            height: 162,
                           }}
-                        />
-                        <div
-                          style={{
-                            textAlign: "center",
-                            fontWeight: "bold",
-                            color: "white",
-                            backgroundColor: "#182339",
-                            borderBottomLeftRadius: 8,
-                            borderBottomRightRadius: 8,
-                          }}
+                          bodyStyle={{ padding: 0 }}
                         >
-                          {champion}
-                        </div>
-                      </Card>
-                    ))}
+                          <img
+                            width={138}
+                            height={138}
+                            src={CHAMPION_IMAGES[champion]}
+                            alt={champion}
+                            key={champion}
+                            style={{
+                              borderTopLeftRadius: 8,
+                              borderTopRightRadius: 8,
+                              objectFit: "cover",
+                              verticalAlign: "middle",
+                            }}
+                          />
+                          <div
+                            style={{
+                              textAlign: "center",
+                              fontWeight: "bold",
+                              color: "white",
+                              backgroundColor: "#182339",
+                              borderBottomLeftRadius: 8,
+                              borderBottomRightRadius: 8,
+                            }}
+                          >
+                            {champion}
+                          </div>
+                        </Card>
+                      ))}
+                    </div>
                   </div>
-                </div>
-              ))}
+                )
+              )}
             </div>
           )}
         </Card>
